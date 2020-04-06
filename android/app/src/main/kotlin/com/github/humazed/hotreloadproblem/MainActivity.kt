97689b73468166c779f6a754870346d680aef262
ibrahim <humazed@gmail.com>
2020-04-06 16:10:29 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/github/humazed/hotreloadproblem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.humazed.hotreloadproblem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+import 'package:hotreloadproblem/page.dart';
+
+import 'map_ui.dart';
+
+final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
+  MapUiPage(),
+];
+
+class MapsDemo extends StatelessWidget {
+  void _pushPage(BuildContext context, GoogleMapExampleAppPage page) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (_) => Scaffold(
+              appBar: AppBar(title: Text(page.title)),
+              body: page,
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('GoogleMaps examples')),
+      body: ListView.builder(
+        itemCount: _allPages.length,
+        itemBuilder: (_, int index) => ListTile(
+          leading: _allPages[index].leading,
+          title: Text(_allPages[index].title),
+          onTap: () => _pushPage(context, _allPages[index]),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(home: MapsDemo()));
+}
